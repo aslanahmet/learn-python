f5df77662d9864e685e92da01e09e0425f949f76
aslanahmet <ahmet@kodeveloper.co>
2017-08-13 20:29:19 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+
+    def function_1(self, number1, number2):
+        return number1 + number2
+
+    def function_2(self, number1, number2):
+        print(self.function_1(number1, number2))
+
---
+++ call.py
@@ -0,0 +1,3 @@
+from calculator import Calculator
+calculator = Calculator()
+calculator.function_2(1, 1)
